549ba45b90b5822547163b550063342ec1886117
jtorrico <jetorrico@gmail.com>
2020-06-11 20:13:58 -0300

    employees stuff first approach

---
+++ src/app/views/employees/components/employees-detail/employees-detail.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Optional, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Employee } from 'src/app/core/models/employee';
+
+@Component({
+  selector: 'app-employees-detail',
+  templateUrl: './employees-detail.component.html',
+  styleUrls: ['./employees-detail.component.scss']
+})
+export class EmployeesDetailComponent implements OnInit {
+
+  public localData: Employee;
+  public name: string;
+
+  constructor(
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: Employee,
+    public dialogRef: MatDialogRef<EmployeesDetailComponent>,
+  ) {
+    this.localData = { ...data };
+    this.name = `${this.localData.firstName} ${this.localData.lastName}`;
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCancel() {
+    this.dialogRef.close({ event: 'Cancel' });
+  }
+  
+}
--- src/app/views/employees/employees.module.ts
+++ src/app/views/employees/employees.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
+import { EmployeesTableComponent } from './components/employees-table/employees-table.component';
+import { EmployeesDetailComponent } from './components/employees-detail/employees-detail.component';
+import { EmployeesFormComponent } from './components/employees-form/employees-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MaterialModule } from 'src/app/material/material.module';
+import { DataTableModule } from 'src/app/data-table/data-table.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeesTableComponent,
+    EmployeesDetailComponent, 
+    EmployeesFormComponent
+  ],
   imports: [
     CommonModule,
-    EmployeesRoutingModule
+    EmployeesRoutingModule,
+    SharedModule,
+    MaterialModule,
+    DataTableModule
   ]
 })
 export class EmployeesModule { }
